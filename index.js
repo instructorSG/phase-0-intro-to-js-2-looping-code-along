230ae96d4941ca7705026eef835372dc0777824a
Sara Ginsburg <sara.ginsburg@flatironschool.com>
2022-04-27 17:00:25 -0400

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let gifts = ['doll', 'computer game', 'bow', 'necklace']
+let messages =[]
+const writeCards = (gifts) => {
+ for (const gift of gifts){
+     messages.push(`Thank you, ${gift}, for the wonderful surprise gift!`)
+
+ }
+ return messages
+}
+
+const countDown = (n) => {
+    while (n>=0){
+        console.log (n)
+        n-=1
+    }
+}
